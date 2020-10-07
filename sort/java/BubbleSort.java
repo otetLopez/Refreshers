1efd7118068c42b36da9610669fa4cb2ba2b601b
otetLopez <otet.tud@gmail.com>
2020-10-06 22:22:07 -0400

    Cleaning Bubble Sort Code

--- sort/java/BubbleSort.java
+++ sort/java/BubbleSort.java
@@ -8,6 +8,7 @@ public class BubbleSort {
     public static void main(String[] args) {
 //        Given that we have an input array
           int[] input = {5, 6, 2, 4, 1};
+
           int[] result = sortIn(input);
           printResult(result);
     }
@@ -27,17 +28,12 @@ public class BubbleSort {
             }
             lastIdx--;
         }
-
-        for (int i=0; i<in.length; i++) {
-            System.out.println("The input["+ i +"]: " + in[i]);
-        }
-
         return in;
     }
+
     private static void printResult(int[] in) {
-        System.out.println("Printing");
         for (int i=0; i<in.length; i++) {
-            System.out.println("The input["+ i +"]: " + in[i] + "\n");
+            System.out.println("The input["+ i +"]: " + in[i]);
         }
     }
 }
